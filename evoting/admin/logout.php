3bf8f57a84f24875439900ee98789fba7f3652bd
ISAAC MURIANI <isaacmuriani@gmail.com>
2026-08-06 16:06:53 +0300

    Delete evoting directory

--- evoting/admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// =============================================================
-//  ADMIN LOGOUT
-//  evoting/admin/logout.php
-// =============================================================
-require_once '../includes/config.php';
-session_destroy();
-redirect('login.php');
-?>
